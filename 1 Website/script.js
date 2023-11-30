6848acb0c5b348df8b206502705116bb9088f565
arunprasadh7 <m.arunprasadh96@gmail.com>
2023-11-30 20:41:24 +0530

    Basics

---
+++ 1 Website/script.js
@@ -0,0 +1,16 @@
+console.log(`Hello `)
+console.log(`I like pizza!`)
+
+window.alert(`This is an alert!`)
+window.alert(`I like pizza`)
+
+document.getElementById('myheading').textContent = 'Hello'
+document.getElementById('my-para').textContent = `I like pizza!`
+
+// this is a comment 
+/* this 
+is 
+multi 
+line 
+comment */
+
